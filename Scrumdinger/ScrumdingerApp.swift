2553b82f44650cc044ec80ff8181b36a04558916
Wole Solana <4244148+wolesolana@users.noreply.github.com>
2024-03-22 15:59:24 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Wole Solana on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
